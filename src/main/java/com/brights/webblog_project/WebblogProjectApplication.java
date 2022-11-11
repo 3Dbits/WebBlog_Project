0b66dc5627eb7cfd354c6f1c11c0ad8a8df40833
Matija Cvetan <matija.cvetan@awacademy.training>
2022-11-11 11:20:07 +0100

    first start up springboot application

---
+++ src/main/java/com/brights/webblog_project/WebblogProjectApplication.java
@@ -0,0 +1,13 @@
+package com.brights.webblog_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebblogProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebblogProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brights/webblog_project/WebblogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brights.webblog_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebblogProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
